c8a300161feda0d79990a597103e41707e8b2e3a
pforsthoff <pforsthoff@gmail.com>
2020-08-15 11:05:55 -0600

    updated deployment with configmap

--- TestNinja/Startup.cs
+++ TestNinja/Startup.cs
@@ -25,8 +25,6 @@ namespace TestNinja
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            LoggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            LoggerFactory.AddDebug();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
