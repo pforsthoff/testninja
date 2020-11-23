db29c12f2e81b2ac511d4838c831bbec3bae0940
Administrator <admin@example.com>
2020-11-23 11:54:11 -0700

    adding to gitlab pipeline

--- TestNinja/Startup.cs
+++ TestNinja/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using log4net;
+using Prometheus;
 
 namespace TestNinja
 {
@@ -26,6 +27,11 @@ namespace TestNinja
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            // Custom Metrics to count requests for each endpoint and the method
+            var counter = Metrics.CreateCounter("peopleapi_path_counter", "Counts requests to the People API endpoints", new CounterConfiguration
+            {
+                LabelNames = new[] { "method", "endpoint" }
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -34,6 +40,14 @@ namespace TestNinja
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            app.Use((context, next) =>
+            {
+                counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+                return next();
+            });
+            // Use the Prometheus middleware
+            app.UseMetricServer();
+            app.UseHttpMetrics();
             app.UseStaticFiles();
 
             app.UseRouting();
